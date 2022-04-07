befd8edd113debbe668b36c26fa9a47fb8648e91
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-07 15:45:34 +0200

    Add: scaffolding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// EXSERCISE
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+// :piccolo_rombo_blu:il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+// :piccolo_rombo_blu:va applicato uno sconto del 20% per i minorenni
+
+// :piccolo_rombo_blu:va applicato uno sconto del 40% per gli over 65.
+
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+// L'output va stampato nella pagina HTML.
+
+// SUBCODE
