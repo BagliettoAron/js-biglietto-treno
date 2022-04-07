59bbe1a0241edc73b3e5a2d7100bc24f29b5d30a
@BagliettoAron <aronbaglietto88@gmail.com>
2022-04-07 15:59:51 +0200

    Add: SUBCODE comments in script.js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,28 @@
 
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 
-// :piccolo_rombo_blu:il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
 
-// :piccolo_rombo_blu:va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 20% per i minorenni
 
-// :piccolo_rombo_blu:va applicato uno sconto del 40% per gli over 65.
+// va applicato uno sconto del 40% per gli over 65.
 
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
 
 // L'output va stampato nella pagina HTML.
 
 // SUBCODE
+
+// input
+// chiedere il numero di km che l'utente intende fare
+// chiedere l'età del passeggero
+
+// script
+// calcolare il prezzo della tratta moltiplicando il numero di km che il cliente intende fare * 0,21€ 
+// se l'età dell'utente è <= di 17 anni applicare 20% di sconto sul prezzo del biglietto
+// se l'età dell'utente è >= 65 anni applicare 40% di sconto sul prezzo del biglietto
+// altrimenti: il prezzo rimane invariato (condizione iniziale della variabile let)
+// approssimare il prezzo finale per un massimo di due cifre decimali dopo la virgola
+
+// output
+// stampare il risultato sulla pagina html
